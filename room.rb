f03f31b3e1d43ec7fd48b916edf1ad081059efde
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-02 14:54:35 +0000

    added booked rooms

--- room.rb
+++ room.rb
@@ -4,11 +4,33 @@ class Room
 
   def initialize(number_of_rooms)
     @number_of_rooms = [1, 2, 3, 4, 5]
-
+    @booked_rooms = []
   end
 
   def number_of_available_rooms()
     return @number_of_rooms.count()
   end
+
+  def number_of_rooms_booked()
+    return @booked_rooms.count()
+  end  
+
+# def check_guests_into_room
+#   @booked_rooms <<
+#   return @booked_rooms.count(
+# end
+
+
+# return @booked_rooms.count
+# # = !nil
+#
+# end
+
+
+
+
+
+
+
 
 end
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -9,6 +9,8 @@ class RoomTest <MiniTest::Test
 
   def setup
   @room = Room.new([1, 2, 3, 4, 5])
+  @guests = Guests.new("Marge")
+  # @booked_rooms = []
   # @guest = Room.new("Rose")
   end
 
@@ -16,5 +18,16 @@ class RoomTest <MiniTest::Test
   def test_check_number_of_available_rooms()
     assert_equal(5, @room.number_of_available_rooms())
   end
+
+  def test_number_of_rooms_booked
+    assert_equal(0, @room.number_of_rooms_booked())
+  end
+
+  # def test_check_guests_into_room()
+  #   # guests = Guests.new("Homer")
+  #   @room.check_guests_into_room(@guests)
+  #   assert_equal(0, @room.check_guests_into_room)
+  #
+  # end
 
 end
