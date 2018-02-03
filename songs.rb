e0d45297e564fbd0749927f9603c632ae5209639
Emma Roberts <emrsixtyseven@gmail.com>
2018-02-03 15:46:19 +0000

    changes to song spec

--- songs.rb
+++ songs.rb
@@ -4,32 +4,52 @@ class Songs
 
   attr_accessor :songs
 
-def initialize(songs)
-  @songs = songs
-
-end
+  def initialize(songs)
+    @songs = songs
+  end
 
   # def initialize(title, artist, genre)
 
-    # @title = title
-    # @artist = artist
-    # @genre = genre
+  # @title = title
+  # @artist = artist
+  # @genre = genre
 
 
-    # @songs = [@title, @artist, @genre]
+  # @songs = [@title, @artist, @genre]
   # end
 
 
-  # def check_song_details()
-  #   return @songs[:title]
-  #
+
+  def get_song_info
+    return @songs.first[:title]
+  end
+
+  # def get_info_from_title
+  #   for song in @songs
+  #     if song[:title] == title
+  #       return song
+  #     end
+  #   end
+  #   return nil
   # end
 
-def get_song_info
-  return @songs.first[:title]
+  # def add_song(title)
+  #   song = {
+  #     title: title,
+  #     artist: "",
+  #     genre: ""
+  #   }
+  #   @songs.push(song)
+  # end
 
-end
-
+# def add_song()
+#   new_song = {
+#       title: "",
+#       artist: "",
+#       genre: ""
+#     }
+#   @song.push(new_song)
+# end
 
 
 end
